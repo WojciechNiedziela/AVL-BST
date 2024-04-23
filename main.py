89db3d5569d56c692ed10c07cb5c317a42cfa71e
root <root@DESKTOP-1G53DPO>
2024-04-23 23:30:04 +0200

    FindMinMax + RemoveAll

--- main.py
+++ main.py
@@ -33,13 +33,26 @@ class Node: #klasa tworząca nowy typ danych - wierzchołek
         if self.right:
             self.right.print_preorder()
 
+    def find_min_max(self):
+        min_val = self.val
+        max_val = self.val
+
+        if self.left:
+            min_val = self.left.find_min_max()[0]
+        if self.right:
+            max_val = self.right.find_min_max()[1]
+
+        return min_val, max_val
+
+
         
         
 def help():
     print("Help         -   Show this message")
+    print("FindMinMax   -   Finding minimum and maximum in the tree")
     print("Print        -   Print the tree using In-order, Pre-order, Post-order")
     print("Remove       -   Remove elements of the tree")
-    print("Delete       -   Delete whole tree")
+    print("RemoveAll    -   Delete whole tree")
     print("Export       -   Export the tree to tikzpicture")
     print("Rebalance    -    Rebalance the tree")
     print("Exit Exits the program (same as ctrl+D)")
@@ -54,6 +67,11 @@ def insert(root, key): #funkcja czytajaca wierzcholek BST i ustawiająca go w od
         else:
             root.left = insert(root.left, key)
     return root
+
+def RemoveAll(root):
+    root = None
+    return root
+
 
 
 def main():
@@ -78,22 +96,30 @@ def main():
             action = input().strip()
             if action == "Help":
                 help()
+            elif action == "FindMinMax":
+                if root is not None:
+                    min_val, max_val = root.find_min_max()
+                    print("Minimum value in the BST is: ", min_val)
+                    print("Maximum value in the BST is: ", max_val)
+                else:
+                    print("Drzewo jest puste")
             elif action == "Print":
-                print("Inorder: ", end="")
-                root.print_inorder()
-                print("\nPostorder: ", end="")
-                root.print_postorder()
-                print("\nPreorder: ", end="")
-                root.print_preorder()
-                print()
+                if root is not None:
+                    print("Inorder: ", end="")
+                    root.print_inorder()
+                    print("\nPostorder: ", end="")
+                    root.print_postorder()
+                    print("\nPreorder: ", end="")
+                    root.print_preorder()
+                    print()
+                else:
+                    print("Drzewo jest puste")
+            elif action == "RemoveAll":
+                root = RemoveAll(root)
+                print("All nodes have been removed from the tree.")
             elif action == "Exit":
                 break
 
-
-        
-        
-        # print("Inorder traversal of the BST is: ")
-        # inorder(root)
 
 if __name__ == "__main__":
     main()
