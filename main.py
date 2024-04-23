c67c1db45e6e1e4d24583bd992433e861abb03ea
root <root@DESKTOP-1G53DPO>
2024-04-23 19:44:09 +0200

    Komentarze wyjaśniające działanie kodu + Help

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+# uruchomienie programu: python program.py --tree BST
+#
+#
+#
+
 import sys
 import argparse
 
-class Node:
-    def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.val = key
+class Node: #klasa tworząca nowy typ danych - wierzchołek
+    def __init__(self, key): # konstruktor pozwalający stworzyc nowy obiekt klasy
+        self.left = None # jego lewe dziecko
+        self.right = None # jego prawe dziecko
+        self.val = key # warttosc wierzcholka
+        
+        
+def help():
+    print("Help Show this message")
+    print("Print Print the tree using In-order, Pre-order, Post-order")
+    print("Remove Remove elements of the tree")
+    print("Delete Delete whole tree")
+    print("Export Export the tree to tikzpicture")
+    print("Rebalance Rebalance the tree")
+    print("Exit Exits the program (same as ctrl+D)")
 
-def insert(root, key):
+
+def insert(root, key): #funkcja czytajaca wierzcholek BST i ustawiająca go w odpowiednim miejscu w zaleznosci od jego wartosci
     if root is None:
         return Node(key)
     else:
@@ -24,9 +40,9 @@ def inorder(root):
         inorder(root.right)
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser() #odpalenie programu przy pomocy komendy
     parser.add_argument("--tree", type=str, help="Type of the tree")
-    args = parser.parse_args()
+    args = parser.parse_args() #
 
     if args.tree == "BST":
         print("nodes> ", end="")
@@ -40,8 +56,14 @@ def main():
         for num in nums:
             root = insert(root, num)
 
-        print("Inorder traversal of the BST is: ")
-        inorder(root)
+        print("action> ", end="")
+        action = input().strip()
+        if action == "Help":
+            help()
+        
+        
+        # print("Inorder traversal of the BST is: ")
+        # inorder(root)
 
 if __name__ == "__main__":
     main()
