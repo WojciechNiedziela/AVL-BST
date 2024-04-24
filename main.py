f6f38caa3b946ad4a029faa99cdff2ec4c51a5e4
root <root@DESKTOP-1G53DPO>
2024-04-25 00:56:33 +0200

    Rebalance

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 # uruchomienie programu: python program.py --tree BST / AVL
-#
-#ToDO:
-#
-#Pomiar czasu - do sprawozdania
-#
-#AVL
-#Implementacja
-#Rebalance
-#
 
-import argparse, time
+import argparse, time, math
 
 def timer_decorator(func):
     def wrapper(*args, **kwargs):
@@ -162,8 +153,48 @@ def insert(root, key): #funkcja czytajaca wierzcholek BST i ustawiająca go w od
     return root
 
 
+def bstToVine(grand: Node) -> int:
+    count = 0
+    tmp = grand.right
+    while tmp:
+ 
+        if tmp.left:
+            oldTmp = tmp
+            tmp = tmp.left
+            oldTmp.left = tmp.right
+            tmp.right = oldTmp
+            grand.right = tmp
 
+        else:
+            count += 1
+            grand = tmp
+            tmp = tmp.right
+ 
+    return count
+ 
+def compress(grand: Node, m: int) -> None:
+    tmp = grand.right
+    for i in range(m):
+        oldTmp = tmp
+        tmp = tmp.right
+        grand.right = tmp
+        oldTmp.right = tmp.left
+        tmp.left = oldTmp
+        grand = tmp
+        tmp = tmp.right
+ 
+def balanceBST(root: Node) -> Node:
+ 
+    grand = Node(0)
+    grand.right = root
+    count = bstToVine(grand)
+    h = int(math.log2(count + 1))
+    m = pow(2, h) - 1
+    compress(grand, count - m)
+    for m in [m // 2**i for i in range(1, h + 1)]:
+        compress(grand, m)
 
+    return grand.right
 
 
 def main():
@@ -248,6 +279,15 @@ def main():
                         print("Export to file tree.tex succeeded")
                     else:
                         print("The tree is empty")
+
+                elif action == "Rebalance":
+                    if root is not None:
+                        root = balanceBST(root)
+                        print("Preorder: ", end="")
+                        root.print_preorder()
+                        print()
+                    else:
+                        print("The tree is empty")
                 
                 elif action == "Exit":
                     break
@@ -270,4 +310,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
